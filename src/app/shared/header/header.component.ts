e20ae9f0000182a96c62be4db676526d10229f36
stevecyril45 <stevechibuikecyril@gmail.com>
2025-09-03 07:52:11 +0100

    commit steve work

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    // Auto-close navbar whenever a route finishes loading
+    this.router.events
+      .pipe(filter((e) => e instanceof NavigationEnd))
+      .subscribe(() => this.closeMenu());
   }
 
+  toggleMenu(): void {
+    const collapseEl = document.getElementById('mainNavbar');
+    const toggler = document.querySelector('.navbar-toggler') as HTMLElement | null;
+
+    if (collapseEl?.classList.contains('show')) {
+      this.closeMenu();
+    } else {
+      collapseEl?.classList.add('show');
+      toggler?.classList.remove('collapsed');
+      toggler?.setAttribute('aria-expanded', 'true');
+    }
+  }
+
+  closeMenu(): void {
+    const collapseEl = document.getElementById('mainNavbar');
+    const toggler = document.querySelector('.navbar-toggler') as HTMLElement | null;
+
+    if (collapseEl?.classList.contains('show')) {
+      collapseEl.classList.remove('show');
+    }
+    toggler?.classList.add('collapsed');
+    toggler?.setAttribute('aria-expanded', 'false');
+  }
 }
